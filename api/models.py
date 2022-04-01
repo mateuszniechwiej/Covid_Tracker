b2062625b36e588b88abe596a2d71129fa67846b
mateuszniechwiej <niechwiej.mateusz@gmail.com>
2022-04-01 08:40:27 +0100

    Create model in api app

--- api/models.py
+++ api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Covid(models.Model):
+    country = models.CharField(max_length=50)
+    code = models.CharField(max_length=10)
+    total_cases =  models.CharField(max_length=50)  
+    total_deaths = models.CharField(max_length=50)
+    total_recorder = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.country
